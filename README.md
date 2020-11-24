98002c9f17d5562db12bb3a1c61b0cb4038ec62c
San Engineer <sanenginer@gmail.com>
2020-11-25 00:38:21 +0700

    update
    add readme and create api documentation with swagger

---
+++ README.md
@@ -0,0 +1,30 @@
+# Create Simple Backend CRUD Application (ReSTAPI) With NodeJS, ExpressJS, And MongoDB
+
+This project I used swagger for documentation and testing ReSTAPI. You don't need post for test your ReSTAPI, I have created example value (include on [swagger_api.json](https://github.com/sanengineer/expressjs-ejs-mongodb/blob/master/package.json))
+
+## 📦️ Prequisite:
+
+- NodeJS 12.18.4 LTS (Recomended)
+- MongoDB
+
+## 📢️ List Package Node Modules For Production:
+
+- ExpressJS
+- Swagger UI Express
+- Mongoose
+- Morgan
+
+## ⚗️ List Package Node Modules For Development
+
+- Dotenv
+- Nodemon
+
+## 📝️ Getting Started Development
+
+Let's fork and clone this project to your local machine!
+
+    $ cd expressjs-mongodb
+
+    $ npm install
+
+    $ npm run dev
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const swaggerui = require("swagger-ui-express");
+const swaggerJson = require("./swagger_api.json");
 const subscribersRouter = require("./routes/subscribers");
 const port = 5500;
+var logger = require("morgan");
 
 require("dotenv").config();
 
@@ -12,24 +15,41 @@ mongoose.connect(process.env.DATABASE_URL, {
 });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("successfully connected to database"));
+db.once("open", () => console.log("\n Successfully Connected To Database\n\n"));
 
+// Configure CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS, DELETE, PUT, POST, PATCH, HEAD"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Authorization, Accept"
+  );
+
+  return next();
+});
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use("/subscribers", subscribersRouter);
+app.use("/api-docs/v1", swaggerui.serve, swaggerui.setup(swaggerJson));
 
 // app.listen(3005, () => console.log("server started"));
 
 // Listen Port
 app.listen(port, () => {
-  console.log("");
   console.log(
-    `\x1b[93mBackend Server now running 🚀 on  http://localhost:${port}\x1b[39m`
+    `\x1b[93m \n Backend Server now running 🚀 on  http://localhost:${port}\x1b[39m\n`,
+    `\x1b[93mBackend Server now running 🚀 on  http://localhost:${port}/api-docs/v1\x1b[39m\n`
   );
   console.log(
-    "\x1b[93mBuild by\x1b[39m \x1b[91mhttps://github.com/sanengineer\x1b[91m"
+    "\x1b[93m Build by\x1b[39m \x1b[91mhttps://github.com/sanengineer\x1b[91m"
   );
   console.log(
-    "\x1b[93mgive ⭐️ start, 🍴 fork and 🧲 clone others repository\x1b[39m"
+    "\x1b[93m give ⭐️ start, 🍴 fork and 🧲 clone others repository\x1b[39m"
   );
   console.log("");
 });
